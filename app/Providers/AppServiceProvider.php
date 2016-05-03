b387c617d8c08344cb1c4c9b4962d8890a68b900
David Bohn <davbohn@googlemail.com>
2016-05-03 13:13:59 +0200

    Removed database related stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,6 @@
 
 namespace App\Providers;
 
-use App\Import\Books\Converter\BookConverter;
-use App\Import\Books\Parser\GrimmParser;
-use App\Import\Books\Parser\SourceParser;
-use App\Import\Books\Parser\TitleParser;
-use App\Import\Books\Parser\YearParser;
-use App\Import\Persons\BioDataExtractor;
-use App\Import\Persons\Converter\PersonConverter;
-use App\Import\Persons\Parser\BioDataParser;
-use App\Import\Persons\Parser\InheritanceParser;
-use App\Import\Persons\Parser\NameParser;
-use App\Import\Persons\Parser\PersonPrintParser;
-use App\Import\Persons\Parser\RestFieldParser as PersonRestFieldParser;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -36,31 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(PersonConverter::class, function () {
-            $converter = new PersonConverter();
 
-            $converter->registerParsers([
-                new NameParser(),
-                new BioDataParser(app(BioDataExtractor::class)),
-                new PersonPrintParser(),
-                new InheritanceParser(),
-                new PersonRestFieldParser(),
-            ]);
-
-            return $converter;
-        });
-
-        $this->app->singleton(BookConverter::class, function () {
-            $converter = new BookConverter();
-
-            $converter->registerParsers([
-                new TitleParser(),
-                new YearParser(),
-                new SourceParser(),
-                new GrimmParser(),
-            ]);
-
-            return $converter;
-        });
     }
 }
