e47bd66d6f77f03645289bebd576c61988abde63
David Bohn <david@cancrisoft.net>
2016-05-03 13:47:09 +0200

    Fixed config

--- config/app.php
+++ config/app.php
@@ -150,7 +150,6 @@ return [
 
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Spatie\Backup\BackupServiceProvider::class,
 
         /*
          * Application Service Providers...
